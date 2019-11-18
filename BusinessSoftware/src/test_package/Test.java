87f1805a6284a733c141003ddcf485267cb9d7ac
Aref <arefobaid6@gmail.com>
2019-11-18 14:06:08 +0100

    test commit

--- BusinessSoftware/src/test_package/Test.java
+++ BusinessSoftware/src/test_package/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test World 18/11/2019");
+		System.out.println("Test World 18/11/2019 test again");
 
 	}
 

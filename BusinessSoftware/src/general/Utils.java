e0ad77a736e415e5c493918143898b073a1ce6a3
omar27 <omar27@w208-51.szut.uni-bremen.de>
2019-12-09 12:23:25 +0100

    added Utils to project

--- BusinessSoftware/src/general/Utils.java
+++ BusinessSoftware/src/general/Utils.java
@@ -2,8 +2,18 @@ package general;
 
 import javax.swing.JFrame;
 
+
+/**
+ * Created on 09.12.2019
+ * @author omar27
+ *
+ */
 public class Utils {
 	
+	/**
+	 * Created by Omar
+	 * @param frame set frames layout
+	 */
 	public static void setOriginalBackgroundSize(JFrame frame) {
 		frame.setBounds(100, 100, 900, 600);
 		frame.setLocationRelativeTo(null);

50e9b7611fa9764327353c6289c547fba1fd585f
omare <omarelhussein9@gmail.com>
2019-12-24 20:36:14 +0100

    merged to aref-v1

--- BusinessSoftware/src/start/Daomelden.java
+++ BusinessSoftware/src/start/Daomelden.java
@@ -6,43 +6,38 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Daomelden {
-	String s="org.sqlite.JDBC";
+	String s = "org.sqlite.JDBC";
 	private final String CONNECTIONSTRING = "jdbc:sqlite:Geaschgeaft.db";
-	
-		  public Daomelden() throws  ClassNotFoundException {
-			// TODO Auto-generated constructor stub
-			  Class.forName(s);
-		}
-		  
-		  
-		  public void insert() {
-			  Connection connection=null;
-			  PreparedStatement preparedStatement=null;
-			  try {
-				  
-				  connection=DriverManager.getConnection(CONNECTIONSTRING);
-				 String sa="INSERT into Abteilung VALUES (?,?,?)";
-				  preparedStatement=connection.prepareStatement(sa);
-				  System.out.println("dsadsadsa");
-				  preparedStatement.setInt(1, 5);
-				  preparedStatement.setString(2, "omar");
-				  preparedStatement.setInt(3, 1);
-				  preparedStatement.execute();
-				  
-				  
-				
-			} catch (SQLException e) {
-				System.out.println(e);
-			}finally {
-				try {
-					preparedStatement.close();
-					connection.close();
-				} catch (Exception e2) {
-					// TODO: handle exception
-				}
-			}
-		  }
 
+	public Daomelden() throws ClassNotFoundException {
+		// TODO Auto-generated constructor stub
+		Class.forName(s);
+	}
+
+	public void insert() {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+
+			connection = DriverManager.getConnection(CONNECTIONSTRING);
+			String sa = "INSERT into Abteilung VALUES (?,?,?)";
+			preparedStatement = connection.prepareStatement(sa);
+			System.out.println("dsadsadsa");
+			preparedStatement.setInt(1, 5);
+			preparedStatement.setString(2, "omar");
+			preparedStatement.setInt(3, 1);
+			preparedStatement.execute();
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				preparedStatement.close();
+				connection.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+		}
+	}
 
 }
-	

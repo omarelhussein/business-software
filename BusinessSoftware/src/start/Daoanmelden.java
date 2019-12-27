b33cb81bdc70f02a74b96cd62851d82ebe67258e
Aref <arefobaid6@gmail.com>
2019-12-28 00:13:16 +0100

    weiter mit Regestierung

---
+++ BusinessSoftware/src/start/Daoanmelden.java
@@ -0,0 +1,14 @@
+package start;
+
+public class Daoanmelden {
+	String s="org.sqlite.JDBC";
+	private final String CONNECTIONSTRING = "jdbc:sqlite:Geaschgeaft.db";
+	
+		  public Daoanmelden() throws  ClassNotFoundException {
+			// TODO Auto-generated constructor stub
+			  Class.forName(s);
+		}
+		  
+		  
+
+}
